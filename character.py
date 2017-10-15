32ecf972a40589465b52f1d223a6f893773119dc
Justin B. Kinney <jkinney@cshl.edu>
2017-10-15 18:09:06 -0400

    Implemented much better character transformation algorithm.

--- character.py
+++ character.py
@@ -1,26 +1,23 @@
 from matplotlib.textpath import TextPath
 from matplotlib.patches import PathPatch
 from matplotlib.colors import to_rgba
-from matplotlib.font_manager import FontProperties 
+
+from matplotlib.transforms import Affine2D, Bbox
+
 import numpy as np
-import os
 import pdb
 
-from utils import Box
-
-
-
 class Character:
-    def __init__(self, c, x, y, w, h, color,
+    def __init__(self, c, xmin, ymin, width, height, color,
                  font_properties=None,
                  flip=False,
                  shade=1,
                  alpha=1, edgecolor='none'):
-        assert w > 0
-        assert h > 0
+        assert width > 0
+        assert height > 0
 
         self.c = c
-        self.box = Box(x, x + w, y, y + h)
+        self.bbox = Bbox.from_bounds(xmin,ymin,width,height)
         self.font_properties = font_properties
         self.flip = flip
 
@@ -37,49 +34,40 @@ class Character:
 
     def draw(self, ax):
 
-        # Define character bounding box
-        bbox = list(self.box.bounds)
-        if self.flip:
-            bbox[2:] = bbox[2:][::-1]
-
         # Draw character
-        put_char_in_box(ax, self.c, bbox, \
+        put_char_in_box(ax, self.c, self.bbox,
+                        flip=self.flip,
                         facecolor=self.color,
                         edgecolor=self.edgecolor,
                         font_properties=self.font_properties)
 
 
 
-def put_char_in_box(ax, char, bbox, facecolor='k', \
+def put_char_in_box(ax, char, bbox, flip=False, facecolor='k',
                     edgecolor='none', 
                     font_properties=None, 
-                    zorder=0, \
-                    font_weight='bold', font_style='normal'):
+                    zorder=0):
 
     # Create raw path
-    path = TextPath((0, 0), char, size=1, prop=font_properties)
+    tmp_path = TextPath((0, 0), char, size=1, prop=font_properties)
 
-    # Get params from bounding box
-    try:
-        set_xlb, set_xub, set_ylb, set_yub = tuple(bbox)
-    except:
-        pdb.set_trace()
-    set_w = set_xub - set_xlb
-    set_h = set_yub - set_ylb
+    # If need to flip character, do it within tmp_path
+    if flip:
+        transformation = Affine2D().scale(sx=1, sy=-1)
+        tmp_path = transformation.transform_path(tmp_path)
 
-    # Rescale path vertices to fit in bounding box
-    num_vertices = len(path._vertices)
-    raw_xs = [x[0] for x in path._vertices]
-    raw_ys = [x[1] for x in path._vertices]
-    raw_xlb = min(raw_xs)
-    raw_ylb = min(raw_ys)
-    raw_w = max(raw_xs) - raw_xlb
-    raw_h = max(raw_ys) - raw_ylb
-    set_xs = set_xlb + (raw_xs - raw_xlb) * (set_w / raw_w)
-    set_ys = set_ylb + (raw_ys - raw_ylb) * (set_h / raw_h)
+    # Get bounding box for temporary character path
+    tmp_bbox = tmp_path.get_extents()
 
-    # Reset vertices of path
-    path._vertices = [100 * np.array([set_xs[i], set_ys[i]]) for i in range(num_vertices)]
+    # THIS IS THE KEY TRANSFORMATION
+    # 1. Translate character path so that lower left corner is at origin
+    # 2. Scale character path to desired width and height
+    # 3. Translate character path to desired position
+    transformation = Affine2D() \
+        .translate(tx=-tmp_bbox.xmin, ty=-tmp_bbox.ymin) \
+        .scale(sx=bbox.width / tmp_bbox.width, sy=bbox.height / tmp_bbox.height) \
+        .translate(tx=bbox.xmin, ty=bbox.ymin)
+    path = transformation.transform_path(tmp_path)
 
     # Make and display patch
     patch = PathPatch(path, facecolor=facecolor, edgecolor=edgecolor,
