5db11262273695f22fe17c406c428206eff8de67
Justin B. Kinney <jkinney@cshl.edu>
2025-01-29 13:47:12 -0500

    Revised syntax in __init__.py

--- logomaker/__init__.py
+++ logomaker/__init__.py
@@ -1,48 +1,56 @@
 # Specify version
-__version__ = '0.8.3' #bumped to 0.8.3 on 2025.01.28
+__version__ = '0.8.4'  # Updated 2025.01.29
 
-# Classes / functions imported with logomaker
-from logomaker.src.Logo import Logo
-from logomaker.src.Glyph import Glyph
-from logomaker.src.Glyph import list_font_names
-from logomaker.src.matrix import transform_matrix
-from logomaker.src.matrix import sequence_to_matrix
-from logomaker.src.matrix import alignment_to_matrix
-from logomaker.src.matrix import saliency_to_matrix
-from logomaker.src.validate import validate_matrix
-from logomaker.src.colors import list_color_schemes
-from logomaker.src.examples import list_example_matrices
-from logomaker.src.examples import get_example_matrix
-from logomaker.src.examples import list_example_datafiles
-from logomaker.src.examples import open_example_datafile
-from logomaker.tests.functional_tests_logomaker import run_tests
-
-# demo functions for logomaker
-import matplotlib.pyplot as plt
+# Standard library imports
 import os
 import re
+import matplotlib.pyplot as plt
+
+# Local imports
+from logomaker.src.Logo import Logo
+from logomaker.src.Glyph import Glyph, list_font_names
+from logomaker.src.matrix import (
+    transform_matrix,
+    sequence_to_matrix,
+    alignment_to_matrix,
+    saliency_to_matrix,
+    validate_matrix
+)
+from logomaker.src.colors import list_color_schemes
+from logomaker.src.examples import (
+    list_example_matrices,
+    get_example_matrix,
+    list_example_datafiles,
+    open_example_datafile
+)
 from logomaker.src.error_handling import check, handle_errors
+from logomaker.tests.functional_tests_logomaker import run_tests
 
 @handle_errors
-def demo(name='fig1b'):
+def demo(name: str = 'fig1b') -> plt.Figure:
     """
     Performs a demonstration of the Logomaker software.
 
     Parameters
     ----------
-    name: (str)
+    name: str
         Must be one of {'fig1b', 'fig1c', 'fig1d', 'fig1e', 'fig1f', 'logo'}.
 
     Returns
     -------
     matplotlib.figure.Figure
         The current matplotlib Figure object.
+
+    Raises
+    ------
+    ValueError
+        If the provided name is not in the list of valid examples.
     """
 
     # build list of demo names and corresponding file names
-    example_dir = '%s/examples' % os.path.dirname(__file__)
+    example_dir = f'{os.path.dirname(__file__)}/examples'
     all_base_file_names = os.listdir(example_dir)
-    example_file_names = ['%s/%s' % (example_dir, temp_name)
+    example_file_names = [f'{example_dir}/{temp_name}'
                      for temp_name in all_base_file_names
                      if re.match(r'demo_.*\.py', temp_name)]
     examples_dict = {}
@@ -52,16 +60,14 @@ def demo(name='fig1b'):
 
     # check that name is valid
     check(name in examples_dict.keys(),
-          'name = %s is not valid. Must be one of %s'
-          % (repr(name), examples_dict.keys()))
+          f'name = {name} is not valid. Must be one of {examples_dict.keys()}')
 
     # open and run example file
     file_name = examples_dict[name]
     with open(file_name, 'r') as f:
         content = f.read()
         line = '-------------------------------------------------------------'
-        print('Running %s:\n%s\n%s\n%s' % \
-              (file_name, line, content, line))
+        print(f'Running {file_name}:\n{line}\n{content}\n{line}')
     exec(open(file_name).read())
 
     # return the current matplotlib Figure object
